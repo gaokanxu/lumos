36daa63618108cc3e691157f3292312e1dadbab5
gaokanxu <gaokanxu@gmail.com>
2024-07-26 16:33:45 +0800

    erase sdk from lumos

--- sdk/program/src/system_program.rs
+++
@@ -1,5 +0,0 @@
-//! The [system native program][np].
-//!
-//! [np]: https://docs.lumoslabs.com/runtime/programs#system-program
-
-crate::declare_id!("11111111111111111111111111111111");
--- sdk/src/wasm/mod.rs
+++
@@ -1,5 +0,0 @@
-//! lumos-sdk Javascript interface
-#![cfg(target_arch = "wasm32")]
-
-pub mod keypair;
-pub mod transaction;
