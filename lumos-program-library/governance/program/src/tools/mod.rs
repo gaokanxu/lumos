0f95ba72a385f5a05334411f70aec657cfa8c816
gaokanxu <gaokanxu@gmail.com>
2024-08-12 21:07:31 +0800

    separate LPL from lumos

--- lumos-program-library/governance/program/src/tools/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Utility functions
-
-pub mod lpl_token;
-
-pub mod bpf_loader_upgradeable;
-
-pub mod pack;
-
-pub mod structs;
--- lumos-program-library/token/confidential-transfer/proof-extraction/src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub mod encryption;
-pub mod errors;
-pub mod transfer;
-pub mod transfer_with_fee;
-pub mod withdraw;
